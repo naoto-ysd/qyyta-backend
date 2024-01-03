8a2ec1f63b88eb8d1efd703cbb7bbc44c17ec09a
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2024-01-04 00:00:27 +0900

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,19 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    if Rails.env.production?
+      # origins TODO write the domain after determining it for the production environment
+    else
+      origins {|source, env| true}
+    end
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: false
+  end
+end
